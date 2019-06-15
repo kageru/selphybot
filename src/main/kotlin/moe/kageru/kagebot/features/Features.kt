f0db8b5c2600a50e4f7c2fa740a03ded2236de34
kageru <kageru@encode.moe>
2019-06-15 12:59:02 +0200

    Put features into separate files

--- src/main/kotlin/moe/kageru/kagebot/features/Features.kt
+++ src/main/kotlin/moe/kageru/kagebot/features/Features.kt
@@ -1,11 +1,6 @@
 package moe.kageru.kagebot.features
 
-import moe.kageru.kagebot.MessageUtil
-import moe.kageru.kagebot.Util
 import moe.kageru.kagebot.config.RawFeatures
-import moe.kageru.kagebot.config.RawWelcomeFeature
-import org.javacord.api.entity.channel.TextChannel
-import org.javacord.api.entity.message.embed.EmbedBuilder
 
 class Features(val welcome: WelcomeFeature?) {
 
@@ -14,18 +9,4 @@ class Features(val welcome: WelcomeFeature?) {
     companion object {
         val NONE = Features(null)
     }
-}
-
-class WelcomeFeature(rawWelcome: RawWelcomeFeature) {
-    val enabled: Boolean = rawWelcome.enabled
-    val embed: EmbedBuilder? by lazy {
-        rawWelcome.content?.let(MessageUtil::mapToEmbed)
-    }
-    val fallbackChannel: TextChannel? = rawWelcome.fallbackChannel?.let {
-        if (rawWelcome.fallbackMessage == null) {
-            throw IllegalArgumentException("[feature.welcome.fallbackMessage] must not be null if fallbackChannel is defined")
-        }
-        Util.findChannel(it)
-    }
-    val fallbackMessage: String? = rawWelcome.fallbackMessage
 }
---
+++ src/main/kotlin/moe/kageru/kagebot/features/WelcomeFeature.kt
@@ -0,0 +1,21 @@
+package moe.kageru.kagebot.features
+
+import moe.kageru.kagebot.MessageUtil
+import moe.kageru.kagebot.Util
+import moe.kageru.kagebot.config.RawWelcomeFeature
+import org.javacord.api.entity.channel.TextChannel
+import org.javacord.api.entity.message.embed.EmbedBuilder
+
+class WelcomeFeature(rawWelcome: RawWelcomeFeature) {
+    val enabled: Boolean = rawWelcome.enabled
+    val embed: EmbedBuilder? by lazy {
+        rawWelcome.content?.let(MessageUtil::mapToEmbed)
+    }
+    val fallbackChannel: TextChannel? = rawWelcome.fallbackChannel?.let {
+        if (rawWelcome.fallbackMessage == null) {
+            throw IllegalArgumentException("[feature.welcome.fallbackMessage] must not be null if fallbackChannel is defined")
+        }
+        Util.findChannel(it)
+    }
+    val fallbackMessage: String? = rawWelcome.fallbackMessage
+}
