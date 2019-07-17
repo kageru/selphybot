cdc053a443ded98d1ff9de91001e6d6f1a4a2c35
kageru <kageru@encode.moe>
2019-07-17 21:37:57 +0200

    Added test for SetConfigFeature
    Closes #10

--- src/test/kotlin/moe/kageru/kagebot/ConfigTest.kt
+++ src/test/kotlin/moe/kageru/kagebot/ConfigTest.kt
@@ -3,7 +3,10 @@ package moe.kageru.kagebot
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.ShouldSpec
+import io.mockk.every
+import io.mockk.mockk
 import moe.kageru.kagebot.config.Config
+import moe.kageru.kagebot.features.SetConfigFeature
 
 class ConfigTest : ShouldSpec({
     TestUtil.prepareTestEnvironment()
@@ -12,5 +15,24 @@ class ConfigTest : ShouldSpec({
         Config.localization shouldNotBe null
         Config.features shouldNotBe null
         Config.commands.size shouldBe 2
+    }
+
+    "should parse test config via command" {
+        val denied = "denied"
+        val testConfig = """
+        [localization]
+        permissionDenied = "$denied"
+        redirectedMessage = "says"
+        messageDeleted = "dongered"
+        
+        [[command]]
+        response = "this command is broken"
+        """.trimIndent()
+        val message = TestUtil.mockMessage("anything")
+        every { message.messageAttachments } returns listOf(mockk {
+            every { url.openStream().readAllBytes() } returns testConfig.toByteArray()
+        })
+        SetConfigFeature().handle(message)
+        Config.localization.permissionDenied shouldBe denied
     }
 })
