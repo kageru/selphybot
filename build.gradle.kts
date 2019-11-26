a5bc947d2776f13989eff1a32670aa37e3f519fe
kageru <kageru@encode.moe>
2019-11-26 14:58:09 +0100

    Update javacord to 3.05

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("com.uchuhimo:konf-core:0.20.0")
     implementation("com.uchuhimo:konf-toml:0.20.0")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.javacord:javacord:3.0.4")
+    implementation("org.javacord:javacord:3.0.5")
     implementation("org.mapdb:mapdb:3.0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
@@ -56,7 +56,7 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
     testImplementation("io.mockk:mockk:1.9.3")
     // these two are needed to access javacord internals (such as reading from sent embeds during tests)
-    testImplementation("org.javacord:javacord-core:3.0.4")
+    testImplementation("org.javacord:javacord-core:3.0.5")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.10.0.pr3")
 }
 
