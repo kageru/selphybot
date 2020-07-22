2b40e0578f17385e81730cfb6722f15c5904acf3
kageru <kageru@encode.moe>
2020-07-22 13:52:20 +0200

    add logging for unwrap operations

--- src/main/kotlin/moe/kageru/kagebot/extensions/ArrowExtensions.kt
+++ src/main/kotlin/moe/kageru/kagebot/extensions/ArrowExtensions.kt
@@ -5,13 +5,17 @@ import arrow.core.Either
 import arrow.core.Tuple3
 import arrow.core.getOrElse
 import arrow.typeclasses.Functor
+import moe.kageru.kagebot.Log
 
 fun <L, R> Either<L, R>.on(op: (R) -> Unit): Either<L, R> {
   this.map { op(it) }
   return this
 }
 
-fun <T> Either<*, T>.unwrap(): T = getOrElse { error("Attempted to unwrap Either.left") }
+fun <T> Either<*, T>.unwrap(): T = getOrElse {
+  Log.warn("Attempted to unwrap $this")
+  error("Attempted to unwrap Either.left")
+}
 
 inline fun <A, B, C, A2, F> Tuple3<A, B, C>.mapFirst(AP: Functor<F>, op: (A) -> Kind<F, A2>) = let { (a, b, c) ->
   AP.run { op(a).map { Tuple3(it, b, c) } }
